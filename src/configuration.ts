5e5ee9afbc1f8ffc2a9b6ece4448d0e7b8433744
MakoreAkaRaku <marcromancolom@gmail.com>
2025-04-01 01:54:07 +0200

    forgot to push object containing all config from env

---
+++ src/configuration.ts
@@ -0,0 +1,13 @@
+import process from 'process';
+
+export const configuration = {
+    port: process.env.PORT ?? 8080,
+    jwt_secret: process.env.JWT_SECRET,
+    database: {
+        url: process.env.DATABASE_URL ?? 'postgres',
+        username: process.env.DATABASE_USERNAME ?? 'postgres',
+        password: process.env.DATABASE_PASSWORD ?? 'postgres',
+        name: process.env.DATABASE_NAME ?? 'postgres',
+    },
+    APP_ROOT: process.env.APP_ROOT ?? "root",
+} as const;
