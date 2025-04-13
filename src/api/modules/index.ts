88361bd42db94c4363a0319fca725dc954c4918b
MakoreAkaRaku <marcromancolom@gmail.com>
2025-04-13 22:23:30 +0200

    updated behavior of module endpoints

--- src/api/modules/index.ts
+++ src/api/modules/index.ts
@@ -1,51 +1,54 @@
-import Elysia, {t, error} from "elysia"; 
-import { registerModule, setModuleGroup, updateModuleAlias } from "../../data/module";
-import { jwtMiddleware } from "../middleware/jwtMiddleware";
+import Elysia, { t, error } from "elysia";
+import { registerModule,moduleIdSchema,updateModuleSchema, userHasOwnershipOfModule, updateModule, getModuleData } from "../../data/module"
 import { peripheral } from "./peripheral";
-import { moduleMiddleware } from "../middleware/moduleMiddleware";
+import { jwtMiddleware } from "../middleware/jwtMiddleware";
+import { hasAdminRole } from "../../data/user";
 
-export const module = new Elysia({prefix: '/module'})
-.post('/register', async ({body}) => {
-      const { tokenAPI } = body
-      const result = await registerModule(tokenAPI)
-      if(!result.valid) {
-        console.error("/module/register failed")
-        return error(401, "This token has already expired or is not correct")
-      }
-      return Response.json({moduleToken: result.body},{status: 201})
+export const module = new Elysia({ prefix: '/module' })
+  .post('/register', async ({ body }) => {
+    const { tokenAPI } = body
+    const result = await registerModule(tokenAPI)
+    if (!result.valid) {
+      console.error("/module/register failed")
+      return error(401, "This token has already expired or is not correct")
     }
+    return Response.json({ moduleToken: result.body }, { status: 201 })
+  }
     ,
     {
       body: t.Object({
-        tokenAPI: t.String({format: 'uuid'})
+        tokenAPI: t.String({ format: 'uuid' })
       })
     })
-    .use(peripheral)
-    .use(moduleMiddleware)
-    .put('/alias', async ({body})=> {
-      const result = await updateModuleAlias(body)
-      if(!result.valid) {
-        return error(401, result.body)
+  .use(peripheral)
+  .use(jwtMiddleware)
+  .guard(
+    {
+      params: moduleIdSchema,
+      beforeHandle: async ({ jwtPayload, params }) => {
+        const config = { userUUID: jwtPayload.uuid, moduleUUID: params.id }
+        if (!hasAdminRole(jwtPayload)) {
+          const hasOwnership = await userHasOwnershipOfModule(config)
+          if (!hasOwnership) return error(401)
+        }
       }
-      return Response.json({body: result.body},{status: 200})
-    }, {
-      body: t.Object({
-        moduleUUID: t.String({format: "uuid"}),
-        newAlias: t.String()
-      }),
-    })
-    .put('/group',async ({body})=> {
-      const moduleConfig = {moduleUUID : body.moduleUUID, groupID: body.groupID}
-      const result  = await setModuleGroup(moduleConfig)
+    },
+    (app) => app
+      .get('/:id', async ({ params }) => {
+        const result = await getModuleData(params.id)
+        if (!result.valid)
+          return error(404,result.msg)
 
-      if(!result.valid) {
-        error(401, "Unauthorized")
-      }
-
-      return new Response(null, {status: 200})
-    }, {
-      body: t.Object({
-        moduleUUID: t.String({format:'uuid'}),
-        groupID: t.Union([t.Integer(),t.Undefined()])
+        return result.body
       })
+      .put('/:id', async ({ params, body }) => {
+        const result = await updateModule(params.id,body)
+        if (!result.valid) {
+          return error(404, result.msg)
+        }
+        return result.body
+      },
+    {
+      body: updateModuleSchema
     })
+  )
--- src/data/module.ts
+++ src/data/module.ts
@@ -1,59 +1,72 @@
-import { modules,users,apiTokens } from "../database/schema"
+import { t, type Static } from "elysia"
+import { modules, apiTokens, peripherals } from "../database/schema"
 import { db } from "../db"
-import { eq,and } from "drizzle-orm"
+import { eq, and } from "drizzle-orm"
+import { createUpdateSchema } from "drizzle-typebox"
 
-export async function registerModule(tokenAPI: string){
+export const moduleIdSchema = t.Object({
+  id: t.String({ format: "uuid" })
+})
+
+
+const moduleSchema = createUpdateSchema(modules)
+
+export const updateModuleSchema = t.Omit(moduleSchema, ['uuid', 'last_seen'])
+
+type UpdateModuleInput = Static<typeof updateModuleSchema>
+
+export async function registerModule(tokenAPI: string) {
   const [row] = await db.insert(modules).values({
     token_api: tokenAPI
   }).returning()
 
-  if(!row) {
-    return {valid : false, body: "Non existent token"} as const
+  if (!row) {
+    return { valid: false, body: "Non existent token" } as const
   }
-  return {valid: true , body: row!.uuid} as const
+  return { valid: true, body: row!.uuid } as const
 }
 
-export async function updateModuleAlias(newConfig : {moduleUUID: string, newAlias: string}) {
-  const {moduleUUID, newAlias} = newConfig
+export async function getModuleData(moduleUIID: string) {
+  const [module] = await db.select()
+    .from(modules)
+    .where(eq(modules.uuid, moduleUIID))
+  if (!module) {
+    return { valid: false, msg: "Module Not Found" } as const
+  }
+
+
+  const modulePeripheral = await db.select()
+    .from(peripherals)
+    .where(eq(peripherals.parent_module,moduleUIID))
+
+  return { valid: true, body: {module, peripherals: modulePeripheral} } as const
+}
+
+
+export async function updateModule(moduleUIID: string, updatedFields: UpdateModuleInput) {
   const [row] = await db.update(modules)
-  .set({ alias: newAlias })
-  .where(eq(modules.uuid,moduleUUID))
-  .returning()
-  if(!row){
-    return {valid: false, body: "Could not update alias"} as const
+    .set(updatedFields)
+    .where(eq(modules.uuid, moduleUIID))
+    .returning()
+
+  if (!row) {
+    return { valid: false, msg: "Failed to update" } as const
   }
-  return {valid: true, body: row!} as const
+
+  return { valid: true, body: row } as const
 }
 
 
-export async function setModuleGroup(moduleConfig :{moduleUUID: string, groupID?: number}) {
-  const [row] = await db.update(modules)
-  .set({belong_group: moduleConfig.groupID})
-  .where(eq(modules.uuid,moduleConfig.moduleUUID))
-  .returning()
-  
-  if(!row) {
-    return {valid: false, body: "Module does not exist"} as const
-  }
-
-  return {valid: true, body: row} as const
-}
-
-
-export async function userHasOwnershipOfModule(request: {userUUID:string, moduleUUID: string}){
-  const [apiTokensFromUserQuery] = await db.select({module_uuid: modules.uuid}).from(modules)
-  .innerJoin(apiTokens,
-    and(
-      eq(modules.uuid,request.moduleUUID),
-      eq(modules.token_api,apiTokens.token_api)
+export async function userHasOwnershipOfModule(request: { userUUID: string, moduleUUID: string }) {
+  const [apiTokensFromUserQuery] = await db.select({ module_uuid: modules.uuid }).from(modules)
+    .innerJoin(apiTokens,
+      eq(modules.token_api, apiTokens.token_api)
     )
-  )
-  .where(eq(apiTokens.user_uuid,request.userUUID))
-  
-  if(!apiTokensFromUserQuery)
-  {
-    return false as const
-  }
-
-  return true as const
+    .where(
+      and(
+        eq(apiTokens.user_uuid, request.userUUID),
+        eq(modules.uuid, request.moduleUUID)
+      )
+    )
+  return !apiTokensFromUserQuery
 }
