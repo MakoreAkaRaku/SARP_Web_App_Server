577538f9b870100d5f144c822be7f11d74e94859
MakoreAkaRaku <marcromancolom@gmail.com>
2025-04-28 21:59:27 +0200

    Added more endpoints for group

--- src/api/group.ts
+++ src/api/group.ts
@@ -1,6 +1,6 @@
 import Elysia, { t, error } from "elysia"
 import { jwtMiddleware } from "./middleware/jwtMiddleware"
-import { registerGroup } from "../data/group"
+import { getGroup, getGroups, registerGroup, deleteGroup, updateGroupSchema, } from "../data/group"
 
 export const group = new Elysia({ prefix: '/group' })
 .use(jwtMiddleware)
@@ -20,6 +20,67 @@ export const group = new Elysia({ prefix: '/group' })
     response: t.Object({}),
     detail: {
       description: 'Create a new group',
+      tags: ['group'],
+    },
+  })
+  .delete('/:id', async ({ jwtPayload,params: { id } }) => {
+    const groupProperties = { id, group_name: jwtPayload.uuid } // Assuming these are the properties of the group
+    const result = await deleteGroup(groupProperties)
+    if (!result.valid) {
+      console.error('/group failed', result.body)
+      throw error(409)
+    }
+    return new Response(null, { status: 204 })
+  }, {
+    params: updateGroupSchema,
+    detail: {
+      description: 'Deletes a group',
+      tags: ['group'],
+    },
+  })
+  .get('/', async ({ jwtPayload }) => {
+    const groupList = await getGroups({ owner_group: jwtPayload.uuid })
+    if (!groupList.valid) {
+      console.error('/group failed', groupList.body)
+      throw error(409)
+    }
+    return groupList.body
+  }, {
+    detail: {
+      description: 'Get all groups from a user',
+      tags: ['group'],
+    },
+  })
+  .get('/:id', async ({ jwtPayload, params : { id} }) => {
+    const groupProperties = { id, owner_group: jwtPayload.uuid } // Assuming these are the properties of the group
+    const result = await getGroup(groupProperties)
+    if (!result.valid) {
+      console.error('/group failed', result.body)
+      throw error(409)
+    }
+    return new Response(null, { status: 204 })
+  }, {
+    params: updateGroupSchema,
+    detail: {
+      description: 'Get a group by its id',
+      tags: ['group'],
+    },
+  })
+  .put('/:id', async ({ jwtPayload, params: { id }, body }) => {
+    const groupProperties = { id, group_name: jwtPayload.uuid } // Assuming these are the properties of the group
+    const result = await deleteGroup(groupProperties)
+    if (!result.valid) {
+      console.error('/group failed', result.body)
+      throw error(409)
+    }
+    return new Response(null, { status: 204 })
+  }, {
+    params: updateGroupSchema,
+    body: t.Object({
+      group_name: t.String(),
+    }),
+    detail: {
+      description: 'Update a group',
       tags: ['group'],
     },
   })
--- src/data/group.ts
+++ src/data/group.ts
@@ -1,18 +1,58 @@
 import { db } from '../db'
 import { groups } from '../database/schema'
-import { eq } from 'drizzle-orm'
-import { createSelectSchema, createInsertSchema } from 'drizzle-typebox'
+import { eq, and, type Update } from 'drizzle-orm'
+import { createSelectSchema, createInsertSchema, createUpdateSchema } from 'drizzle-typebox'
 import { t, type Static } from 'elysia'
+import { group } from '../api/group'
 
 
-const insertGroup = createInsertSchema(groups)
+const insertSchema = createInsertSchema(groups)
+const updateSchema = createUpdateSchema(groups, {id: t.Integer()})
 
-const insertGroupSchema = t.Omit(insertGroup, ['id'])
-type Group = Static<typeof insertGroupSchema>
+export const insertGroupSchema = t.Omit(insertSchema, ['id'])
+type InsertGroupSchema = Static<typeof insertGroupSchema>
 
-export async function registerGroup(newGroup: Group) {
+export const updateGroupSchema = t.Omit(updateSchema, ['group_name', 'owner_group'])
+type DeleteGroupSchema = Static<typeof updateSchema>
+
+export async function registerGroup(newGroup: InsertGroupSchema) {
   try {
     const [row] = await db.insert(groups).values(newGroup).returning()
+    if (!row) {
+      return { valid: false, body: "Group already exists" } as const
+    }
+    return { valid: true, body: row } as const
+  } catch (error) {
+    return { valid: false, body: 'conflict' } as const
+  }
+}
+
+export async function getGroups(user: { owner_group: string }) {
+  const groupList = await db.select().from(groups).where(eq(groups.owner_group, user.owner_group))
+  if (!groupList) {
+    return { valid: false, msg: "Error on Query" } as const
+  }
+  return { valid: true, body: groupList } as const
+}
+
+export async function getGroup(group: { id: number, owner_group: string }) {
+  const groupList = await db.select().from(groups).where(
+    and(
+      eq(groups.id, group.id),
+      eq(groups.owner_group, group.owner_group)
+    ))
+  if (!groupList) {
+    return { valid: false, msg: "Error on Query" } as const
+  }
+  return { valid: true, body: groupList } as const
+}
+
+export async function deleteGroup(removedGroup: DeleteGroupSchema) {
+  try {
+    const [row] = await db.delete(groups)
+    .where(
+      eq(groups.id,removedGroup.id)
+    ).returning()
     if (!row) {
       return { valid: false, body: "Group already exists" } as const
     }
