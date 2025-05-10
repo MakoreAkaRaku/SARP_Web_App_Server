dbd78d872dd0911b430d5a3afa7fdd4175a1d82e
MakoreAkaRaku <marcromancolom@gmail.com>
2025-05-10 23:28:29 +0200

    added backend url to the configuration file

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,6 +3,7 @@ import process from 'process';
 export const configuration = {
     port: process.env.PORT ?? 8080,
     jwt_secret: process.env.JWT_SECRET,
+    backend_url: process.env.BACKEND_URL ?? 'http://localhost:3030',    
     database: {
         url: process.env.DATABASE_URL ?? 'postgres',
         username: process.env.DATABASE_USERNAME ?? 'postgres',
