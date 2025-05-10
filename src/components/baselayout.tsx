3a9cb639c12b0d9d045525016db00d040a8335e9
MakoreAkaRaku <marcromancolom@gmail.com>
2025-05-10 23:28:47 +0200

    added horizontal navbar to the base layout

--- src/components/baselayout.tsx
+++ src/components/baselayout.tsx
@@ -1,4 +1,5 @@
 import {Html} from "@elysiajs/html"
+import HorizontalNavBar from "./navbar"
 
 export interface BaseLayoutProps {
   title?: string
@@ -11,11 +12,14 @@ export default function BaseLayout({ title, children }: BaseLayoutProps) {
     <html lang="es">
       <head>
         <meta charset="UTF-8" />
+        {/* FIX ME PLEASE */}
+        <link rel="icon" href="/src/components/pages/resources/SARP_leaf.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{pageTitle}</title>
         <link rel="stylesheet" href="public/stylesheet.css" />
       </head>
       <body class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
+        <HorizontalNavBar/>
         {children}
       </body>
     </html>
