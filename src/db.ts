e432ad37a75221e66fc9b10a003f7d30418174c0
MakoreAkaRaku <marcromancolom@gmail.com>
2025-05-10 23:28:01 +0200

    removal of ;

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,8 @@
-import {drizzle} from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './database/schema';
+import {drizzle} from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as schema from './database/schema'
 
-import {configuration} from './configuration';
+import {configuration} from './configuration'
 
 const postgress =  postgres(configuration.database.url);
 export const db = drizzle({ client: postgress, schema });
