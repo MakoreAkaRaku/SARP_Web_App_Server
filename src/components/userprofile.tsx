2d43eb423dff21e4e3a9f393a82c147a3d1d9067
MakoreAkaRaku <marcromancolom@gmail.com>
2025-06-19 16:47:07 +0200

    fix issue regarding tailwind size-12 in prod for UserProfile Icon

--- src/components/userprofile.tsx
+++ src/components/userprofile.tsx
@@ -5,7 +5,7 @@ import { IconDefaultUser } from "../pages/resources/resources"
 export function UserProfile(user: { username:string, userRole: number }) {
   return (
     <a href="/profile" class="flex flex-row gap-x-4 items-center border-green-700 rounded-md hover:bg-gray-600 hover:border-green-500 shadow-md px-2 border-x-2">
-      <IconDefaultUser classes="size-12" />
+      <IconDefaultUser classes="size-10" />
       <div class="flex text-center flex-col">
         <span>{user.username}</span>
         <span class="text-gray-500">{userPermissions.at(user.userRole-1)}</span>
