1f0f94200cd6bd14259a95ca2721b9f9626b5485
MakoreAkaRaku <marcromancolom@gmail.com>
2025-08-27 20:21:04 +0200

    label class and label input are now default themed

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -48,8 +48,8 @@ export default function Input({
   name,
   value,
   classContainer = "mb-5",
-  classLabel = "block mb-2 text-sm font-medium text-gray-900 dark:text-white",
-  classInput = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+  classLabel = "block mb-2 text-sm font-medium text-white dark:text-white",
+  classInput = "bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 border text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
   ...rest }: InputProps) {
   return (
     <div class={classContainer}>
